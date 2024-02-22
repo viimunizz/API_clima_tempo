d94f5391e7c7614667c4113daff2d525175c69ab
viimunizz <vitoriamuniz0507@gmail.com>
2024-02-22 15:10:28 -0300

    API

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const axios = require('axios');
+const path = require ('path');
+const cors = require ('cors');
+const config =  require('./config.jsonconst');
+const apikey = config.apikey;
+
+const app = express();
+app.listen(3000);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(_dirname, 'public')));
+
+const traducaoClima = {
+    "few clouds": "Poucas nuvens",
+    "scatterd clouds": "Nuvens dispersas"
+}
+
+app.get('?climatempo/:cidade', async (req, res)=> {
+    const city = req.params.parametro;
+
+try{
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
+    if(response.status === 200){
+
+        const clima = traducaoClima[response.data.weather[0].description] || response.data.weather[0].description
+
+        const weatherData = {
+            Temperatura: response.data.main.temp,
+            Umidade: response.data.main.humidity,
+            VelocidadeDoVento: response.data.wind.speed,
+            Clima: clima
+        };
+
+        res.send(weatherData);
+    } else{
+        res.status(response.status).send({erro: 'Erro ao obter dados meteorológiocos'})
+    }
+
+} catch (error){
+    res.status(500).send({erro: 'Erro ao obter dados meteorológiocs', error })
+}
+});
+
