b9acfc1effecde12549d33649169b2c0d1bcbcf6
viimunizz <vitoriamuniz0507@gmail.com>
2024-02-22 17:19:18 -0300

    api

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const axios = require('axios');
-const path = require ('path');
-const cors = require ('cors');
-const config =  require('./config.jsonconst');
+const path = require('path');
+const cors = require('cors');
+const config = require('./config.json');
 const apikey = config.apikey;
 
 const app = express();
@@ -10,36 +10,44 @@ app.listen(3000);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const traducaoClima = {
     "few clouds": "Poucas nuvens",
-    "scatterd clouds": "Nuvens dispersas"
+    "scattered clouds": "Nuvens dispersas",
+    "overcast clouds": "Nublado",
+    "clear sky": "Céu limpo",
+    "light rain": "Chuva leve",
+    "moderate rain": "Chuva moderada"
+
 }
 
-app.get('?climatempo/:cidade', async (req, res)=> {
-    const city = req.params.parametro;
+app.get('/climatempo/:cidade', async (req, res) => {
 
-try{
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
-    if(response.status === 200){
+    const city = req.params.cidade;
 
-        const clima = traducaoClima[response.data.weather[0].description] || response.data.weather[0].description
+    try {
 
-        const weatherData = {
-            Temperatura: response.data.main.temp,
-            Umidade: response.data.main.humidity,
-            VelocidadeDoVento: response.data.wind.speed,
-            Clima: clima
-        };
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
 
-        res.send(weatherData);
-    } else{
-        res.status(response.status).send({erro: 'Erro ao obter dados meteorológiocos'})
+        if (response.status === 200) {
+
+            const clima = traducaoClima[response.data.weather[0].description] || response.data.weather[0].description;
+    
+            const weatherData = {
+                Temperatura: response.data.main.temp,
+                Umidade: response.data.main.humidity,
+                VelocidadeDoVento: response.data.wind.speed,
+                Clima: clima
+            };
+
+            res.send(weatherData);
+        } else {
+            res.status(response.status).send({ erro: 'Erro ao obter dados meteorológiocos' })
+        }
+
+    } catch (error) {
+        res.status(500).send({ erro: 'Erro ao obter dados meteorológiocs', error })
     }
-
-} catch (error){
-    res.status(500).send({erro: 'Erro ao obter dados meteorológiocs', error })
-}
 });
 
