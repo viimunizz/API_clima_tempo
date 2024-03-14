c36b88c745f8e4f0bf74383dfd7f85b74367d2cd
viimunizz <vitoriamuniz0507@gmail.com>
2024-03-14 13:55:14 -0300

    api.v

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const axios = require('axios');
-const path = require('path');
-const cors = require('cors');
-const config = require('./config.json');
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
+const cors = require("cors");
+const config = require("./config.json");
 const apikey = config.apikey;
 
 const app = express();
@@ -10,44 +10,37 @@ app.listen(3000);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const traducaoClima = {
-    "few clouds": "Poucas nuvens",
-    "scattered clouds": "Nuvens dispersas",
-    "overcast clouds": "Nublado",
-    "clear sky": "Céu limpo",
-    "light rain": "Chuva leve",
-    "moderate rain": "Chuva moderada"
+app.get("/climatempo/:cidade", async (req, res) => {
+  const city = req.params.cidade;
 
-}
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&appid=${apikey}&units=metric`
+    );
+    if (response.status === 200) {
+      const clima = response.data.weather[0].description;
+      const climaFormatado = clima.charAt(0).toUpperCase() + clima.slice(1);
+      const icon = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`;
+      const flag = `https://flagsapi.com/${response.data.sys.country}/flat/64.png`;
 
-app.get('/climatempo/:cidade', async (req, res) => {
+      const watherData = {
+        Temperatura: response.data.main.temp_min,
+        Umidade: response.data.main.humidity,
+        VelocidadeDoVento: response.data.wind.speed,
+        Clima: climaFormatado,
+        Icone: icon,
+        Flag: flag,
+      };
 
-    const city = req.params.cidade;
-
-    try {
-
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
-
-        if (response.status === 200) {
-
-            const clima = traducaoClima[response.data.weather[0].description] || response.data.weather[0].description;
-    
-            const weatherData = {
-                Temperatura: response.data.main.temp,
-                Umidade: response.data.main.humidity,
-                VelocidadeDoVento: response.data.wind.speed,
-                Clima: clima
-            };
-
-            res.send(weatherData);
-        } else {
-            res.status(response.status).send({ erro: 'Erro ao obter dados meteorológiocos' })
-        }
-
-    } catch (error) {
-        res.status(500).send({ erro: 'Erro ao obter dados meteorológiocs', error })
+      res.send(watherData);
+    } else {
+      res
+        .status(response.status)
+        .send({ erro: "Erro ao obter dados metereológicos" });
     }
+  } catch (error) {
+    res.status(500).send({ erro: "Erro ao obter dados metereológicos", error });
+  }
 });
-
